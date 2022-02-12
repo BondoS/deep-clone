61693bb144998acf27dfc46456c9600da62138de
Serag <eng.serag00@gmail.com>
2022-02-12 20:37:39 +0100

    Create deepCloneAdvanced

--- deepClone.mjs
+++ deepClone.mjs
@@ -1,13 +1,28 @@
 /*
  * Basic deep clone, this will not work in case of copying
- *  a function or circular references or....
+ *  a function or circular references.
  */
-// export function deepCloneBasic( originalObj) {
-//  return JSON.parse (JSON.stringify (originalObj));
-
-// }
 
 export const deepCloneBasic = (originalObj) =>
   JSON.parse(JSON.stringify(originalObj));
 
-// export deepCloneBasic;
+/*
+ * Advanced deep clone, this will handle Objects, Arrays and fix circular reference issue.
+ */
+export const deepCloneAdvanced = (originalObj, map = new WeakMap()) => {
+  if (typeof originalObj === 'object') {
+    // Handle Object or Array types.
+    let clone = Array.isArray(originalObj) ? [] : {};
+    // Fixing circular reference issue by returning the reference of an object if it was saved from previous iteration.
+    if (map.get(originalObj)) return map[originalObj];
+    // Use weakMap to save a copy of current Object reference in memory.
+    map.set(originalObj, originalObj);
+    for (const key in originalObj) {
+      // recursively call deepCloneAdvanced function to traverse through the whole object.
+      clone[key] = deepCloneAdvanced(originalObj[key], map);
+    }
+    return clone;
+  }
+  // basic/exit condition
+  return originalObj;
+};
--- index.mjs
+++ index.mjs
@@ -1,4 +1,4 @@
-import { deepCloneBasic } from './deepClone.mjs';
+import { deepCloneAdvanced, deepCloneBasic } from './deepClone.mjs';
 import { test } from './test.mjs';
 
 // test
@@ -8,13 +8,26 @@ import { test } from './test.mjs';
       child1: {
         child2: 'msg from child2',
         sibling2: "I'm the sibling",
+        siblingArr: [1, 3, 5, 7],
       },
     },
+    func: () => 'test func',
   };
-  const copiedObj = deepCloneBasic(originalObj);
+
+  const copiedObjBasic = deepCloneBasic(originalObj);
+
+  // intentionally create a circular reference issue
+  // which will cause a "Maximum call stack size exceeded" error
+  originalObj.circularReferenceBug = originalObj;
+  const copiedObjAdvanced = deepCloneAdvanced(originalObj);
+
   originalObj.parent.child1.child2 = 'msg changed';
 
   test('deepCloneBasic', () =>
     originalObj.parent.child1.child2 === 'msg changed' &&
-    copiedObj.parent.child1.child2 === 'msg from child2');
+    copiedObjBasic.parent.child1.child2 === 'msg from child2');
+
+  test('deepCloneAdvanced', () =>
+    copiedObjAdvanced.parent.child1.child2 === 'msg from child2' &&
+    copiedObjAdvanced.func() === 'test func');
 })();
